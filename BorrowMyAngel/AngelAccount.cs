ff364cf0f053f373639091f1e1f4ebf5fb13601b
Jace Medlin <jacemedlin@gmail.com>
2018-11-03 00:49:16 -0500

    Created UserAccount & AngelAccount

---
+++ BorrowMyAngel/AngelAccount.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace BorrowMyAngel {
+    class AngelAccount : UserAccount{
+        private string lName;
+        private string mName;
+        private string address;
+        private int phone;
+        private List<string> references;
+
+        private string reasons;
+        private string crisisExperience;
+        private string volunteerExperience;
+        private bool backgroundCheckPermission;
+        private string convictions;
+
+        public AngelAccount(string userID, string email, string password, string lName, string mName, string address, int phone, List<string> references, string reasons, string crisisExperience, string volunteerExperience, bool backgroundCheckPermission, string convictions) :
+            base(userID, email, password){
+            this.lName = lName;
+            this.mName = mName;
+            this.address = address;
+            this.phone = phone;
+            this.references = references;
+            this.reasons = reasons;
+            this.crisisExperience = crisisExperience;
+            this.volunteerExperience = volunteerExperience;
+            this.backgroundCheckPermission = backgroundCheckPermission;
+            this.convictions = convictions;
+        }
+    }
+}
---
+++ BorrowMyAngel/UserAccount.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace BorrowMyAngel {
+    public class UserAccount {
+        private string userID;
+        private string email;
+        private string password;
+
+        private string fName;
+        private string nickname;
+        private int age;
+        private char gender;
+        private string city;
+        private string state;
+
+        public UserAccount(string userID, string email, string password) {
+            this.userID = userID;
+            this.email = email;
+            this.password = password;
+        }
+
+        public string UserID { get => userID; set => userID = value; }
+        public string Email { get => email; set => email = value; }
+        public string Password { get => password; set => password = value; }
+        public string FName { get => fName; set => fName = value; }
+        public string Nickname { get => nickname; set => nickname = value; }
+        public int Age { get => age; set => age = value; }
+        public char Gender { get => gender; set => gender = value; }
+        public string City { get => city; set => city = value; }
+        public string State { get => state; set => state = value; }
+    }
+}
