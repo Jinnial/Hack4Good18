8ad307189447ce91da441bddcb0ca91ef5aa00d9
Tiffany Ford <rhysma@gmail.com>
2018-11-03 20:42:07 -0500

    new items for guest dashboard

--- BorrowMyAngel/GuestDashboardActivity.cs
+++ BorrowMyAngel/GuestDashboardActivity.cs
@@ -18,8 +18,19 @@ namespace BorrowMyAngel
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Dashboard);
 
-            // Create your application here
+            Button startCall = FindViewById<Button>(Resource.Id.startCall);
+            startCall.Click += StartCall_Click;
+        }
+
+
+        private void StartCall_Click(object sender, EventArgs e)
+        {
+            var intent = new Intent(this, typeof(AudioCallActivity));
+            StartActivity(intent);
+
+
         }
     }
 }
